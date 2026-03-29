d5d5129b368910efacf5c2be364035b55bb24e94
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-29 15:51:50 +0530

    Update n_copies.rb

--- n_copies.rb
+++ n_copies.rb
@@ -1,1 +1,12 @@
+puts "Enter a string:"
+str = gets.chomp
 
+puts "Enter number of times:"
+n = gets.chomp.to_i
+
+if n >= 0
+  result = str * n
+  puts "Result: #{result}"
+else
+  puts "Enter a non-negative number"
+end
