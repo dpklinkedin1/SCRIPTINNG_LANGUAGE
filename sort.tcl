0587bf0ac47935b4dfba7d35525c17b4a16ee245
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-30 03:35:28 +0530

    Update sort.tcl

--- sort.tcl
+++ sort.tcl
@@ -19,3 +19,26 @@ set sorted_list [lsort -command compare $mylist]
 
 puts "Original List: $input"
 puts "Sorted List: $sorted_list"
+
+
+
+
+#--------------------------------------------------
+proc compare_desc {a b} {
+    if {$a > $b} {
+        return -1
+    } elseif {$a < $b} {
+        return 1
+    } else {
+        return 0
+    }
+}
+
+puts "Enter numbers separated by spaces:"
+gets stdin input
+
+puts "Before sorting: $input"
+
+set sorted [lsort -command compare_desc $input]
+
+puts "After sorting: $sorted"
