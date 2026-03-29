d0bb923042184f4bedfc4cc1fae2595f1555937a
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-29 16:37:09 +0530

    Update circle.rb

--- circle.rb
+++ circle.rb
@@ -1,1 +1,12 @@
+def circle_calculation(r)
+  area = Math::PI * r * r
+  perimeter = 2 * Math::PI * r
 
+  puts "Area = #{area}"
+  puts "Perimeter = #{perimeter}"
+end
+
+puts "Enter radius:"
+r = gets.chomp.to_f
+
+circle_calculation(r)
