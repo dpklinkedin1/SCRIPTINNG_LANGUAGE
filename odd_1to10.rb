f29b0665aa8e272328d91f8742cfa06e9d47ab9c
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-29 16:57:00 +0530

    Update odd_1to10.rb

--- odd_1to10.rb
+++ odd_1to10.rb
@@ -2,3 +2,30 @@ puts "odd numbers from 1 to 10:"
  10.downto(1) do |i|
      print"\t#{i}"if i.odd?
 end 
+
+
+
+
+
+#------------------------------------
+9.downto(1).step(2) do |i|
+  puts i
+end
+
+
+
+
+
+
+#------------------------------------------
+
+(1..20).each do |i|
+  puts i if i.odd?
+end
+
+
+#-----------------------for even 
+puts "odd numbers from 1 to 10:"
+ 10.downto(1) do |i|
+     print"\t#{i}"if i.even?
+end 
