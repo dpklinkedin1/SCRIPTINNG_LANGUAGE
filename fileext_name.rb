63cdd58de01a01594605d3a2d2da9b082fd649f6
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-30 01:10:24 +0530

    Update fileext_name.rb

--- fileext_name.rb
+++ fileext_name.rb
@@ -1,3 +1,26 @@
+print "Enter file name: "
+path = gets.chomp
+
+if File.exist?(path)
+  puts "Full Path: #{File.expand_path(path)}"
+
+  if File.file?(path)
+ 	puts "File exists"
+    puts "Type: File"
+    puts "Extension: #{File.extname(path)}"
+  else
+    puts "Type: Folder"
+  end
+else
+  puts "Path does not exist."
+end
+
+
+#------------------------------------------------------------------
+
+
+
+
 puts "Enter the file name:"
 ext = gets.chomp.split(".")
 puts ext[-1]
