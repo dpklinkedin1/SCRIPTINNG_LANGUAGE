186a5a76c9d013565c860ccb19875d93572c8fac
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-04-09 09:24:09 +0530

    Update list_4operations.tcl

--- list_4operations.tcl
+++ list_4operations.tcl
@@ -1,1 +1,15 @@
+set list1 {1 2 3}
+lappend list1 4
+lappend list1 5
 
+set list2 {6 7}
+
+puts "Traversing list1:"
+foreach element $list1 {
+    puts $element
+}
+
+set final_list [concat $list1 $list2]
+
+puts "Concatenated list:"
+puts $final_list
