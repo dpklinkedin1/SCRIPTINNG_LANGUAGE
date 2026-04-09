27c0b6500d4bf2352cd9f7e246cc1b11ef49a657
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-04-09 09:35:49 +0530

    Update file_copy_scr_dest.tcl

--- file_copy_scr_dest.tcl
+++ file_copy_scr_dest.tcl
@@ -1,62 +1,19 @@
-#!/usr/bin/tclsh
-
 puts "Enter source file:"
-gets stdin src
+set src [gets stdin]
 
 puts "Enter destination file:"
-gets stdin dest
+set dest [gets stdin]
 
 set in [open $src r]
-set out [open $dest w]
+fconfigure $in -translation auto
 
-while {[gets $in line] >= 0} {
-    puts $out $line
-}
+set out [open $dest w]
+fconfigure $out -translation auto
+
+set data [read $in]
+puts -nonewline $out $data
 
 close $in
 close $out
 
-puts "File copied to $dest in native format."
-
-
-
-
-
-#------------------------------------------------------
-
-
-
-
-
-
-puts "Enter source file:"
-gets stdin src
-
-puts "Enter destination file:"
-gets stdin dest
-
-# check if source exists
-if {![file exists $src]} {
-    puts "Error: $src not found!"
-} else {
-    file copy $src $dest
-    puts "File copied from $src to $dest"
-}
-
-
-
-```
-
-### 📤 Output:
-```
-Enter source file:
-hello.txt
-Enter destination file:
-output.txt
-File copied from hello.txt to output.txt
-
-
-
-
-
-#----------------------------------------------------
+puts "File copied and translated to native format."
